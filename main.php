3f2f13b8a90ac4f0a6a5fe81673b1e6bd1bf7930
Nishant Aggarwal <naggarwal@wesleyan.edu>
2023-10-12 15:39:13 -0400

    basic registration complete

---
+++ main.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Main Page</title>
+    <meta name="description" content="." />
+
+
+</head>
+<body>
+    Main Page
+</body>
+</html>
